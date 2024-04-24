4144bf752bc23e10efe5165b19b9b5ae66495288
Stunhammer <139661852+Stunhammer@users.noreply.github.com>
2024-04-24 13:50:39 -0400

    Add files via upload

--- Python game/Python game.py
+++ Python game/Python game.py
@@ -1,16 +1,12 @@
-import textwrap
-import Rooms
-import Player
-import Commands
 import cmd
-import Rooms
-showFullExits = True
 import textwrap
-NORTH = 'NORTH'
-SOUTH = 'SOUTH'
-EAST = 'EAST'
-WEST = 'WEST'
-DESC = 'DESC'
+showFullExits = True
+
+NORTH = 'north'
+SOUTH = 'south'
+EAST = 'east'
+WEST = 'west'
+DESC = 'desc'
 currentRoom = 'Main Room'
 SCREEN_WIDTH = 80
 
@@ -58,12 +54,11 @@ def displayRooms(room):
         if direction in worldRooms[room].keys():
             exits.append(direction.title())
     print()
-    if showFullExits:
-        for direction in (NORTH, SOUTH, EAST, WEST):
-            if direction in worldRooms[room]:
-                print('%s: %s' % (direction.title(), worldRooms[currentRoom][direction]))
-            else:
-                print('Exits: %s' % ' '.join(exits))
+    print('Available Exits: ')
+    print('----------------\n')
+    for direction in (NORTH, SOUTH, EAST, WEST):
+        if direction in worldRooms[room]:
+            print('%s: %s' % (direction.title(), worldRooms[currentRoom][direction]))
 
 
 class TextPuzzleCmd(cmd.Cmd):
@@ -78,6 +73,7 @@ class TextPuzzleCmd(cmd.Cmd):
 
     def do_quit(self, arg):
         """Quit the game"""
+        print('Thank you for playing')
         return True
 
     def do_north(self, arg):
@@ -133,7 +129,6 @@ if __name__ == '__main__':
     print(textwrap.fill('----------------------------------------'
                         '----------------------------------------', SCREEN_WIDTH))
     print()
-    print('Type "start" to begin the game')
+    print('Type "start" to begin the game, additional commands can be found with "help".')
     displayRooms(currentRoom)
     TextPuzzleCmd().cmdloop()
-    print('Thank you for playing the game.')
